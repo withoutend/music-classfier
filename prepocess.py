9ca0f3dee3936da0656dc313929d40afb8335744
withoutend <withoutbeginningorend@gmail.com>
2019-01-06 09:20:23 +0800

    Add files via upload

--- prepocess.py
+++ prepocess.py
@@ -10,16 +10,23 @@ from os import path
 import wave
 #import seaborn
 
-def fft_array(filename,channels=2,frame_length=40,overlap=10,bit_depth=16,white_noise_std_in_dB=2**10):
+def fft_array(filename,channels=2,frame_length=40,overlap=10,bit_depth=16,white_noise_std_in_dB=2**10,interval=[0,-1]):
     mag=[]
     phase=[]
     fs, data = wavfile.read(filename+'.wav')
     
     #compute average of all channel
-    square_sum=[item for item in data.T[0]]
-    for i in range(1,channels):
-        square_sum+=[item for item in data.T[i]]
-    rms=np.array([(item/channels) for item in square_sum])
+    if(interval[1]==-1):
+        #if(interval[0]>=0 and interval[0]<=(data.T[0].size/fs):
+        square_sum=[item for item in data.T[0][int(fs*interval[0]):]]
+        for i in range(1,channels):
+            square_sum+=[item for item in data.T[i]]
+        rms=np.array([(item/channels) for item in square_sum][int(fs*interval[0]):])
+    else:
+        square_sum=[item for item in data.T[0][int(fs*interval[0]):int(fs*interval[1])]]
+        for i in range(1,channels):
+            square_sum+=[item for item in data.T[i]]
+        rms=np.array([(item/channels) for item in square_sum][int(fs*interval[0]):int(fs*interval[1])])
     
     #add white noise
     rms+=np.random.normal(0, (2**bit_depth)*(10**(white_noise_std_in_dB/10)), size=rms.shape[0])
@@ -86,7 +93,7 @@ def ifft_array(mag,phase,channels=2,frame_length=40,overlap=10,bit_depth=16,fs=4
     #get real part, convert to 16 bit-depth
     return np.array([item*channels for item in data],dtype=np.int16)
 
- def low_pass_filter(mag,phase,cut_off=500,decay_in_dB=-10,frame_length=40,overlap=10,bit_depth=16,fs=44100):
+def low_pass_filter(mag,phase,cut_off=500,decay_in_dB=-10,gain_in_dB=-3,frame_length=40,overlap=10,bit_depth=16,fs=44100):
     num_sample=(mag[0].size-1)*2
     
     #compute frequency resolution
@@ -98,6 +105,7 @@ def ifft_array(mag,phase,channels=2,frame_length=40,overlap=10,bit_depth=16,fs=4
     
     #transfer dB into ratio
     decay=10**(decay_in_dB/10)
+    gain=10**(gain_in_dB/10)
     for i in range(mag[0].size):
         f=i*f_resolution
         if(f>=cut_off):
@@ -109,11 +117,11 @@ def ifft_array(mag,phase,channels=2,frame_length=40,overlap=10,bit_depth=16,fs=4
         #do noting on phase
         lpf_phase.append(0)
         
-    return np.multiply(mag,np.array(lpf_mag)),np.array(phase+np.array(lpf_phase))
+    return np.multiply(mag*gain,np.array(lpf_mag)),np.array(phase+np.array(lpf_phase))
 
 '''
-mag,phase=fft_array('untitled',white_noise_std_in_dB=-100)
-mag,phase=low_pass_filter(mag,phase,cut_off=20000,decay_in_dB=-10)
+mag,phase=fft_array('untitled',white_noise_std_in_dB=-10,interval=[0,0.5])
+mag,phase=low_pass_filter(mag,phase,cut_off=1000,decay_in_dB=-10)
 array=ifft_array(mag,phase)
 wavfile.write('tem.wav',rate=44100,data=array[0:int(len(array)/2)])
 '''
